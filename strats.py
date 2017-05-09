f3c11e117c90bbd1596e81f77efbf005d24fb71f
tiwanll <thisiswhataninjalookslike@gmail.com>
2017-05-09 09:43:37 -0400

    Added strats

--- strats.py
+++ strats.py
@@ -17,11 +17,41 @@ def random(h, oh1, oh2):
 
 ## Cooperate
 ## Always cooperate
-def alwaysc(h,oh1,oh2):
+def alwaysc(h, oh1, oh2):
 	return 'c'
 
 ## Defect
 ## Always defect
-def alwaysd(h,oh1,oh2):
+def alwaysd(h, oh1, oh2):
 	return 'd'
 
+## Alternate
+## Cooperate, then defect, then cooperate, then defect...
+def alternate(h, oh1, oh2):
+	return 'c' if len(h)%2==0 else 'd'
+
+## Grudger
+## Cooperate until one defects, then defect forever
+def grudger(h, oh1, oh2):
+	return 'd' if ('d' in oh1) or ('d' in oh2) else 'c'
+
+## Grudger2
+## Cooperate until both defect, then defect forever
+def grudger2(h, oh1, oh2):
+	return 'd' if ('d' in oh1) and ('d' in oh2) else 'c'
+
+## AntiGrudger
+## Defect until one cooperates, then cooperate forever
+def agrudger(h, oh1, oh2):
+	return 'c' if ('c' in oh1) or ('c' in oh2) else 'd'
+
+## AntiGrudger2
+## Defect until both cooperate, then cooperate forever
+def agrudger2(h, oh1, oh2):
+	return 'c' if ('c' in oh1) and ('c' in oh2) else 'd'
+
+def randomtft(h, oh1, oh2):
+	if len(h)!=0:
+		return oh1[-1] if random()<0.5 else oh2[-1]
+	else:
+		return 'c'
